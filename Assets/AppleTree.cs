9e35a691a564025a5b792fc19b43c930b9dc4188
Aidan Varkoly <47114022+AidanVarkoly@users.noreply.github.com>
2019-09-05 12:44:33 -0400

    Update #1
    Put in the movement of the tree

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    public GameObject appleprefab;
+
+    public float speed = 1f;
+
+    public float LeftandRightEdge = 10f;
+
+    public float ChanceToChangeDirections = 0.1f;
+
+    public float SecondsBetweenAppleDrops = 1f;
+
+    // Dropping Apples every second
+    void Start()
+    { 
+        
+    }
+
+    void Update()
+    {
+        // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Changing Direction
+        if ( pos.x < -LeftandRightEdge )
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > LeftandRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Random.value < ChanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+}
